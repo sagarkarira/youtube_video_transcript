c6b570e7d4b8e7829caaabdf224a4dc857f16274
Sagar Karira <sagarkarira1992@gmail.com>
2016-10-04 21:38:52 +0530

    added body-parser and empty transcript check

--- app.js
+++ app.js
@@ -4,25 +4,33 @@ var ejs 	= require('ejs');
 var request = require('request');
 var fs = require('fs');
 var xml2js = require('xml2js');
-
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 
 app.get('/', function(req, res) {
 	res.render('pages/index');
 });
 
 app.post('/ajax', function(req, res) {
-	// console.log(req.body);
-	// var url = req.body.url;
-	var url = 'http://video.google.com/timedtext?lang=en&v=1A_CAkYt3GY';
 
+	var url = req.body.url;
+	var id = url.slice(-11);
+	url = 'http://video.google.com/timedtext?lang=en&v=' + id;
+	console.log("Fetching transcript for-" + url);
 	request(url, function(error, response, html){
 		if (error) {
 			console.log(error);
 			return res.send("Something went wrong");
+		}
+		//console.log("Html-"+html)
+		if (html.length == 0) {
+			console.log("Transcript does not exist");
+			return res.send("transcript does not exist");
 		}
 		var parser = new xml2js.Parser();
 		var xml = html;
