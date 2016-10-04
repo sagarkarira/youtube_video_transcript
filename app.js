92af68d03ce408899a54a163740833feb56e5348
Sagar Karira <sagarkarira1992@gmail.com>
2016-10-04 21:16:35 +0530

    added gui

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
 'use strict';
+var express = require('express');
+var ejs 	= require('ejs');
 var request = require('request');
 var fs = require('fs');
 var xml2js = require('xml2js');
 
-var url = 'http://video.google.com/timedtext?lang=en&v=1A_CAkYt3GY';
 
-request(url, function(error, response, html){
-	var parser = new xml2js.Parser();
-	var xml = html;
-	parser.parseString(xml, function (err, result) {
-   		var textArray = result.transcript.text;
-   		console.log(textArray);
-   		for (var i in textArray) {
-			var obj = textArray[i];
-   			console.log(obj['_'].replace('\\n',' ').replace('--', ' '));
-   		}
+var app = express();
 
-   	
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+app.post('/ajax', function(req, res) {
+	// console.log(req.body);
+	// var url = req.body.url;
+	var url = 'http://video.google.com/timedtext?lang=en&v=1A_CAkYt3GY';
+
+	request(url, function(error, response, html){
+		if (error) {
+			console.log(error);
+			return res.send("Something went wrong");
+		}
+		var parser = new xml2js.Parser();
+		var xml = html;
+		parser.parseString(xml, function (err, result) {
+			if (error) {
+				console.log(error);
+				return res.send("Something went wrong");
+			}
+	   		var textArray = result.transcript.text;
+	   		//console.log(textArray);
+			var subs = '';
+	   		for (var i in textArray) {
+				var obj = textArray[i];
+	   			var subs = subs + '\n' +  obj['_'].replace('\\n',' ').replace('--', ' ');
+			}
+			res.send(subs);
+		});
 	});
 });
+
+app.listen(8080);
+console.log('Server started');
